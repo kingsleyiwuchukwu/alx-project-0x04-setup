aab6870e97903ba673c3e24e4a5caa3dbb31e028
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-01 23:51:34 +0100

    Include a Redux for global state management

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
 import Button from "../common/Button";
+import { usePathname } from "next/navigation";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
 
   const pathname = usePathname()
-  const { count } = useCount()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
-    <header className="fixed w-full bg-white shadow-md p-8 z-50">
+    <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-4xl md:text-5xl font-bold list-none tracking-tight">
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
         </Link>
 
--- alx-project-0x06/pages/counter-app.tsx
+++ alx-project-0x06/pages/counter-app.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+import { useSelector } from "react-redux";
+import { RootState, useAppDispatch, AppDispatch, increment, decrement } from "@/store/store";
 
 const CounterApp: React.FC = () => {
-  const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch: AppDispatch = useAppDispatch()
 
-  const decrement = () => {
-    setCount(count > 0 ? count - 1 : 0);
-  };
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-yellow-400 to-pink-500 flex flex-col justify-center items-center text-white">
@@ -29,20 +25,20 @@ const CounterApp: React.FC = () => {
       {/* Buttons */}
       <div className="flex space-x-4">
         <button
-          onClick={increment}
+          onClick={() => dispatch(increment())}
           className="bg-green-500 hover:bg-green-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 shadow-lg transform hover:scale-105"
         >
           Increment 🚀
         </button>
         <button
-          onClick={decrement}
+          onClick={() => dispatch(decrement())}
           className="bg-red-500 hover:bg-red-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 shadow-lg transform hover:scale-105"
         >
           Decrement 👎
         </button>
       </div>
 
-      {/* Footer message. */}
+      {/* Footer message */}
       <p className="mt-8 text-sm text-white opacity-75">
         Keep clicking, who knows what happens at 100? 😏
       </p>
