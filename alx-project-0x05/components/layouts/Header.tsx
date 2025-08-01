9078f589fc86cb3665f1f22e76c86fe5b8d26ce1
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-01 23:26:22 +0100

    Include a ContextAPI for global state management

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,16 +1,38 @@
 import Link from "next/link";
-import Button from "@/components/common/Button";
+import { usePathname } from "next/navigation";
+import { useCount } from "@/context/CountContext";
+import Button from "../common/Button";
 
 const Header: React.FC = () => {
+
+  const pathname = usePathname()
+  const { count } = useCount()
+
   return (
-    <header className="fixed w-full bg-white shadow-md">
+    <header className="fixed w-full bg-white shadow-md p-8 z-50">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+        <Link href="/" className="text-4xl md:text-5xl font-bold list-none tracking-tight">
           Splash App
         </Link>
+
+        {/* Button Group */}
         <div className="flex gap-4">
-          <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-          <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
